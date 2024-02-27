d43c39a52fdf1d6a3886ba49906c375f539a85bb
Pietra <pietra.felipe@portalsesisp.org.br>
2024-02-27 08:10:35 -0300

    Envio

---
+++ 12. vetor.js
@@ -0,0 +1,38 @@
+const numeros = [1, 2, 3, 4, 5];
+console.log(numeros);
+console.log(numeros[3]);
+
+numeros[2] = 6;
+console.log(numeros);
+console.log(numeros[2]);
+console.log("--------------------------------------------------");
+
+var dinos = ["tiranossauro rex", "estegossauro", "anquilossauro"];
+console.log(dinos[0]);
+console.table(dinos);
+console.log("o vetor tem", dinos.length, "elementos!");
+
+dinos.push("brontossauro", "melacirapitor");
+console.log(dinos);
+
+dinos.unshift("tricoratops");
+console.table(dinos);
+
+// alterando elementos com base no indice (posição)
+dinos[3] = "titanossauro";
+console.table(dinos);
+
+// obtendo um elemento com base em seus indice
+console.log("primeira posição:", dinos[0]);
+console.log("segunda posição:", dinos[1]);
+console.log("item de posição 20:", dinos[19]);
+
+// removendo item do vetor
+dinos.pop();
+console.table(dinos);
+
+dinos.shift();
+console.table(dinos);
+
+dinos.splice(0, 1);
+console.table(dinos);
